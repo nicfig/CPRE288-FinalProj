fdb76fe1eb5ea0130068dc3651f62a197da362fb
Cose <ajcose@iastate.edu>
2022-05-02 18:53:00 -0500

    dajfa

--- FinalProject/Final Project Code Files/main.c
+++ FinalProject/Final Project Code Files/main.c
@@ -91,10 +91,54 @@ int main(void) {
 	    else {
 	        oi_setWheels(0, 0);
 	    }
+
+	    int cCheck = cliffCheck(sensor);
+
+	    if(cCheck == 1){
+	        oi_setWheels(0, 0);
+	        uart_sendStr("c1");
+	    }
+	    else if(cCheck == 2){
+	        oi_setWheels(0, 0);
+	        uart_sendStr("c2");
+	    }
+	    else if(cCheck == 3){
+	        oi_setWheels(0, 0);
+	        uart_sendStr("c3");
+	    }
+	    else if(cCheck == 4){
+	        oi_setWheels(0, 0);
+	        uart_sendStr("c4");
+	    }
 	}
 
     oi_free(sensor);
 
 	return 0;
 
+}
+
+int cliffCheck(oi_t *sensor){
+    oi_update(sensor);
+
+    int cliffStatus = 0;
+
+    if(sensor -> cliffLeft){
+        cliffStatus = 1;
+    }
+    else if(sensor -> cliffRight){
+        cliffStatus = 2;
+    }
+    else if(sensor -> cliffFrontLeft){
+        cliffStatus = 3;
+    }
+    else if(sensor -> cliffFrontRight){
+        cliffStatus = 4;
+    }
+    else{
+        cliffStatus = 0;
+    }
+    lcd_printf("In check:  %d", cliffStatus);
+    //timer_waitMillis(5000);
+    return cliffStatus;
 }
